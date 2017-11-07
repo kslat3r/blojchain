3ecb241880c6f02a0f11e4503176c958b5db6486
Ed Kelly <ekelly@sapient.com>
2017-11-07 17:52:28 +0000

    Swagger doc

--- src/server/middleware/swagger-document.js
+++ src/server/middleware/swagger-document.js
@@ -1,5 +1,12 @@
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load(`${__dirname}/../app-definition.yaml`);
+const swaggerJSDoc = require('swagger-jsdoc');
 
-module.exports = swaggerUi.setup(swaggerDocument);
+module.exports = swaggerUi.setup(swaggerJSDoc({
+  swaggerDefinition: {
+    info: {
+      title: 'Blojchainj',
+      version: '1.0.0',
+    },
+  },
+  apis: ['../routes/*.js'],
+}));
