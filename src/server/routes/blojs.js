7a14b49d38971df36c57f05e00785fea1511bf80
Ed Kelly <ed@newlinelabs.com>
2017-11-12 22:24:31 +0000

    Add swagger

--- src/server/routes/blojs.js
+++ src/server/routes/blojs.js
@@ -4,25 +4,14 @@ const chain = require('../../lib/chain');
 
 /**
  * @swagger
- * /login:
- *   post:
- *     description: Login to the application
+ * /blojs:
+ *   get:
+ *     description: Get all blojs
  *     produces:
  *       - application/json
- *     parameters:
- *       - name: username
- *         description: Username to use for login.
- *         in: formData
- *         required: true
- *         type: string
- *       - name: password
- *         description: User's password.
- *         in: formData
- *         required: true
- *         type: string
  *     responses:
  *       200:
- *         description: login
+ *         description: Blojs
  */
 router.get('/', function(req, res) {
   res.send(chain.get());
