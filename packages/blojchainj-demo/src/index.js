c1e4ce035ad68ac91be0fb76284c42e56d53c388
Ed Kelly <ed@newlinelabs.com>
2017-12-09 23:15:25 +0000

    Remove worker

--- packages/blojchainj-demo/src/index.js
+++ packages/blojchainj-demo/src/index.js
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { default as registerServiceWorker }  from './registerServiceWorker';
 import { configureStore } from './store';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 
 dotenv.config()
-registerServiceWorker();
 
 const store = configureStore();
 
