3656b3fd9eab58bf3c878497dea440e8e1cd888f
Ed Kelly <ed@newlinelabs.com>
2017-12-23 22:03:39 +0000

    Logger

--- packages/demo/src/components/Logger.js
+++ packages/demo/src/components/Logger.js
@@ -8,21 +8,45 @@ class Logger extends Component {
     logs: PropTypes.array.isRequired,
   }
 
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
   componentDidUpdate() {
     this.elem.scrollTop = this.elem.scrollHeight;
   }
 
+  toggle(e) {
+    e.preventDefault();
+
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
   render() {
     return (
-      <div className="logger" ref={(elem) => { this.elem = elem; }}>
-        {this.props.logs.map((log, i) => {
-          return (
-            <Log
-              key={i}
-              log={log}
-            />
-          )
-        })}
+      <div className={`logger ${this.state.open && 'open'}`} ref={(elem) => { this.elem = elem; }}>
+        <a href="#" onClick={this.toggle}>
+          {!this.state.open ? 'Maximise' : 'Minimise'}
+        </a>
+
+        <div className="scroll">
+          {this.props.logs.map((log, i) => {
+            return (
+              <Log
+                key={i}
+                log={log}
+              />
+            )
+          })}
+        </div>
       </div>
     );
   }
