ec7f04abba09404c586662969ad753b06f410658
Ed Kelly <kslat3r@potato.local>
2020-11-28 16:34:52 +0000

    CircleCI

--- app/node/helm/install.sh
+++ app/node/helm/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm upgrade --install blojchain-node-a -f blojchain-node-a/values.yaml blojchain-node
-helm upgrade --install blojchain-node-b -f blojchain-node-b/values.yaml blojchain-node
-helm upgrade --install blojchain-node-c -f blojchain-node-c/values.yaml blojchain-node
-helm upgrade --install blojchain-node-d -f blojchain-node-d/values.yaml blojchain-node
+helm upgrade --install blojchain-node-a -f blojchain-node-a/values.yaml --set image.tag=$CIRCLE_SHA1 blojchain-node
+helm upgrade --install blojchain-node-b -f blojchain-node-b/values.yaml --set image.tag=$CIRCLE_SHA1 blojchain-node
+helm upgrade --install blojchain-node-c -f blojchain-node-c/values.yaml --set image.tag=$CIRCLE_SHA1 blojchain-node
+helm upgrade --install blojchain-node-d -f blojchain-node-d/values.yaml --set image.tag=$CIRCLE_SHA1 blojchain-node
