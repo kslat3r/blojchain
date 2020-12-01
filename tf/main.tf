7acfbed66d485d2af39f8c866595054dc892a4d7
Ed Kelly <me@edkelly.co.uk>
2020-12-01 14:18:10 +0000

    Monorepo CircleCI

--- tf/main.tf
+++ tf/main.tf
@@ -17,3 +17,44 @@ resource "google_container_cluster" "blojchain-cluster" {
     oauth_scopes = local.oauth_scopes
   }
 }
+
+resource "google_dns_managed_zone" "blojchain" {
+  name     = "blojchain"
+  dns_name = "blojcha.in."
+}
+
+resource "google_dns_record_set" "blojchain-demo" {
+  name         = "demo.blojcha.in."
+  managed_zone = google_dns_managed_zone.blojchain.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["35.242.133.184"]
+}
+
+resource "google_dns_record_set" "blojchain-node-a" {
+  name         = "node-a.blojcha.in."
+  managed_zone = google_dns_managed_zone.blojchain.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["35.189.75.125"]
+}
+
+resource "google_dns_record_set" "blojchain-node-b" {
+  name         = "node-b.blojcha.in."
+  managed_zone = google_dns_managed_zone.blojchain.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["35.230.153.63"]
+}
+
+resource "google_dns_record_set" "blojchain-node-c" {
+  name         = "node-c.blojcha.in."
+  managed_zone = google_dns_managed_zone.blojchain.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = ["34.89.47.127"]
+}
